a03cf50c3c312f62481701d3524442f50e63a0ac
NotReiny on Minecraft <142988995+NotReiny@users.noreply.github.com>
2025-10-26 18:53:31 +0700

    Create s.js

---
+++ static/s.js
@@ -0,0 +1,30 @@
+let a = ```   File  Edit  View  Search  Run  Debug  Options                          Help 
+┌───────────────────────────────── Untitled ───────────────────────────────┤↑├─┐
+│                                                                              ↑
+│                                                                             
+│                                                                              ░
+│           ┌────────────────────────────────────────────────────────┐         ░
+│           │                                                        │        ░
+│           │                Welcome to MS-DOS QBasic                │        ░
+│           │                                                        │        ░
+│           │     Copyright (C) Microsoft Corporation, 1987-1992.    │        ░
+│           │                  All rights reserved.                  │        ░
+│           │                                                        │        ░
+│           │       <
+Press Enter to see the Survival Guide >        │        ░
+│           ├────────────────────────────────────────────────────────┤        ░
+│           │         < Press ESC to clear this dialog box >         │        ░
+│           └────────────────────────────────────────────────────────┘        ░
+│                                                                            ░
+│                                                                              ░
+│                                                                              ░
+│                                                                              ↓
+│← ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░→│
+├───────────────────────────────── Immediate ──────────────────────────────────┤
+│                                                                              │
+│                                                                              │
+F1=Help   Enter=Execute   Esc=Cancel   Tab=Next Field   Arrow=Next Item       ```
+```
+
+a = a.replaceAll(" ","\u00A0")
+document.getElementsByTag("p")[0].innerHTML = a
