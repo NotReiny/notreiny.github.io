5802694aa1cd051af55a57cccb5de0683f1d2888
NotReiny on Minecraft <142988995+NotReiny@users.noreply.github.com>
2025-10-26 18:56:02 +0700

    Update s.js

--- static/s.js
+++ static/s.js
@@ -24,7 +24,6 @@ Press Enter to see the Survival Guide >        │        ░
 │                                                                              │
 │                                                                              │
 F1=Help   Enter=Execute   Esc=Cancel   Tab=Next Field   Arrow=Next Item       ```
-```
 
 a = a.replaceAll(" ","\u00A0")
 document.getElementsByTag("p")[0].innerHTML = a
