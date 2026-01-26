f21d86ae529436627765ca0ae4b63782d41eecaa
NotReiny on Minecraft <142988995+NotReiny@users.noreply.github.com>
2026-01-26 13:33:35 +0700

    Update inflo.js

--- static/inflo.js
+++ static/inflo.js
@@ -1,8 +1,31 @@
 class inflo {
     static isNum = /^(?<s>[+-])?(?:(?<i>\d+)(?:\.(?<f>\d*))?|\.(?<f2>\d+))(?:[Ee](?<es>[+-])?(?<e>\d+))?$/;
-    static prec = 21n;
+    static prec = 20n;
     static pow10 = 10n ** inflo.prec;
     static pow10_n = inflo.pow10 * 10n;
+
+    static PI = new inflo("3.141592653589793")
+    static TAU = new inflo("6.283185307179586")
+    static SQRT2 = new inflo("1.4142135623730951")
+    static SQRT3 = new inflo("1.7320508075688772")
+    static SQRT5 = new inflo("2.23606797749979")
+    static GOLDEN_RATIO = new inflo("1.618033988749895")
+    static SILVER_RATIO = new inflo("2.414213562373095")
+    static CBRT2 = new inflo("1.2599210498948732")
+    static ROOT12_2 = new inflo("1.0594630943592953")
+    static SUPERGOLDEN_RATIO = new inflo("1.4655712318767682")
+    static E = new inflo("2.718281828459045")
+    static LN2 = new inflo("0.6931471805599453")
+    static RAMANUJANS_CONSTANT = new inflo("262537412640767700")
+    static GELFONDS_CONSTANT = new inflo("23.140692632779263")
+    static GELFOND_SCHNEIDER_CONSTANT = new inflo("2.665144142690225")
+    static TRIBONACCI_CONSTANT = new inflo("1.8392867552141612")
+
+    static LN10 = new inflo("2.302585092994046")
+    static LOG10E = new inflo("0.4342944819032518")
+    static LOG2E = new inflo("1.4426950408889634")
+    static SQRT1_2 = new inflo("0.7071067811865476")
+
     constructor(inp) {
         const s = typeof inp === "string" ? inp : inp.toString();
         const ss = s.trim();
@@ -89,60 +112,88 @@ class inflo {
         return this.man > b.man ? 1 : (this.man < b.man ? -1 : 0);
     }
     sqrt() {
-        if (this.isz) return new inflo(0);
+        if (this.isz) return new inflo("0");
         if (this.man < 0n) throw new Error("not a number");
         let a = this.__copy__();
-        let old_a = new inflo(0);
+        let prevA = new inflo("-1");
         a.e = (a.e + inflo.prec) / 2n - inflo.prec;
-        while (a.compare(old_a)) {
-            old_a = a;
+        while (a.compare(prevA)) {
+            prevA = a.__copy__();
             a = (new inflo(1).divide(2)).times(a.plus(this.divide(a)));
         }
         return a;
     }
-ePow() {
-    if (this.isz) return new inflo(1);
-    
-    // 1. Handle negative exponents: e^(-x) = 1 / e^x
-    if (this.man < 0n) {
-        return new inflo(1).divide(this.__negate__().ePow());
+    ePow() {
+        if (this.isz) return new inflo(1);
+
+        // 1. Handle negative exponents: e^(-x) = 1 / e^x
+        if (this.man < 0n) {
+            return new inflo(1).divide(this.__negate__().ePow());
+        }
+
+        // 2. Argument Reduction (Scaling and Squaring)
+        // We want to scale 'x' down until it is small (e.g., < 0.5)
+        let k = 0n;
+        let x = this.__copy__();
+        const threshold = new inflo("0.5");
+
+        while (x.compare(threshold) > 0) {
+            x = x.divide(2);
+            k++;
+        }
+        // 3. Taylor Series for the reduced 'x'
+        // e^x = 1 + x + x^2/2! + x^3/3! ...
+        let term = new inflo("1");
+        let sum = new inflo("1");
+        let i = 1n;
+
+        while (true) {
+            // term = term * x / i
+            term = term.times(x).divide(i);
+            let prevSum = sum.__copy__();
+            sum = sum.plus(term);
+
+            // If the sum stops changing within our precision, we are done
+            if (sum.compare(prevSum) === 0) break;
+            i++;
+        }
+
+        // 4. Squaring back up: (e^(x/2^k))^(2^k)
+        for (let j = 0n; j < k; j++) {
+            sum = sum.times(sum);
+        }
+
+        return sum;
     }
 
-    // 2. Argument Reduction (Scaling and Squaring)
-    // We want to scale 'x' down until it is small (e.g., < 0.5)
-    let k = 0n;
-    let x = this.__copy__();
-    const threshold = new inflo("0.5");
-    
-    while (x.compare(threshold) > 0) {
-        x = x.divide(2);
-        k++;
+    ln() {
+        let sum = new inflo("0");
+        let prevSum = new inflo("-1");
+
+        let z = this.__copy__();
+
+        // If input is greater than 1, Do not run otherwise it crashes.
+        if (z.compare("0") < 1) throw new Error("result is undefined");
+        z = z.minus("1").divide(z.plus("1"))
+
+        let zs = z.times(z);
+        let i = 0;
+
+
+        while (sum.compare(prevSum)) {
+            prevSum = sum.__copy__();
+            sum = sum.plus(z.divide(new inflo(i).times("2").plus("1")));
+            z = z.times(zs);
+            i++;
+        }
+        sum = sum.times("2");
+
+        return sum;
     }
-
-    // 3. Taylor Series for the reduced 'x'
-    // e^x = 1 + x + x^2/2! + x^3/3! ...
-    let term = new inflo(1);
-    let sum = new inflo(1);
-    let i = 1n;
-
-    while (true) {
-        // term = term * x / i
-        term = term.times(x).divide(i);
-        let oldSum = sum.__copy__();
-        sum = sum.plus(term);
-
-        // If the sum stops changing within our precision, we are done
-        if (sum.compare(oldSum) === 0) break;
-        i++;
+    log10() {
+        let a = this.__copy__();
+        return a.ln().divide(new inflo("10").ln());
     }
-
-    // 4. Squaring back up: (e^(x/2^k))^(2^k)
-    for (let j = 0n; j < k; j++) {
-        sum = sum.times(sum);
-    }
-
-    return sum;
-}
     toString() {
         if (this.isz) return "0";
         // 1. Get the absolute mantissa and the sign
@@ -174,6 +225,15 @@ ePow() {
         const rest = s.slice(1);
         const expSign = trueExp >= 0 ? "" : ""; // optional: standard plus sign
         return `${sign}${firstDigit}${rest ? "." + rest : ""}e${expSign}${trueExp}`;
+    }
+    static recomputeConstants() {
+        inflo.SQRT2 = new inflo("2").sqrt()
+        inflo.SQRT3 = new inflo("3").sqrt()
+        inflo.SQRT5 = new inflo("5").sqrt()
+        inflo.GOLDEN_RATIO = new inflo("5").sqrt().plus("1").divide("2")
+        inflo.SILVER_RATIO = new inflo("2").sqrt().plus("1")
+
+        inflo.SQRT1_2 = new inflo("1").divide("2").sqrt()
     }
     __copy__() {
         const x = Object.create(inflo.prototype);
