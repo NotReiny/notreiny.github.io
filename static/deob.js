d77df9cb7fb02c1e73d2e07ed1ce43b7531d05b8
NotReiny on Minecraft <142988995+NotReiny@users.noreply.github.com>
2026-03-27 18:57:17 +0700

    Update deob.js

--- static/deob.js
+++ static/deob.js
@@ -3498,58 +3498,58 @@ try{
                             var h = t.l;
                             if (!xt(h, s)) {
                                 var y = Math.round(10 * t.rawx * v)
-                                    , g = Math.round(20 * t["rawy"] * v);
-                                t["highlighted"] && (E["fillStyle"] = "rgba(239, 255, 71, 0.5)",
-                                    E["fillRect"](y - 2 * v, g - 2 * v, Math["ceil"](r) + 4 * v, Math.round(a) + 4 * v));
+                                    , g = Math.round(20 * t.rawy * v);
+                                t.highlighted && (E.fillStyle = "rgba(239, 255, 71, 0.5)",
+                                    E.fillRect(y - 2 * v, g - 2 * v, Math.ceil(r) + 4 * v, Math.round(a) + 4 * v));
                                 var p = t.c;
                                 var anonIdShow = document.getElementById('anonIdShow').checked;
-                                tt.disablecolour["checked"] && (p = 0),
-                                    0 == p && xe && (p = se["length"]),
-                                    E["fillStyle"] = me[p],
+                                tt.disablecolour.checked && (p = 0),
+                                    0 == p && xe && (p = se.length),
+                                    E.fillStyle = me[p],
                                     kt(y, g, r, a),
-                                    !tt["shownametags"]["checked"] || (i = h,
+                                    !tt.shownametags.checked || (i = h,
                                         c = void 0,
                                         l = void 0,
                                         u = void 0,
                                         c = n,
-                                        l = 20 * Math[c(437)](i[0] / 20) + "," + 10 * Math.floor(i[1] / 10),
-                                        (u = we[c(520)](l)) && u.protected && 0 == j) || Mt(t.n != "" || !anonIdShow ? t.n : `(0)`, y, g, o)
+                                        l = 20 * Math.floor(i[0] / 20) + "," + 10 * Math.floor(i[1] / 10),
+                                        (u = we.get(l)) && u.protected && 0 == j) || Mt(t.n != "" || !anonIdShow ? t.n : `(0)`, y, g, o)
                             }
                         }
                     }
-                    for (var b = 0; b < Ne["length"]; b++) {
-                        0 == (p = Ne[b][3]) && xe && (p = se["length"]),
-                            E["fillStyle"] = me[p]["replace"]("0.2", Ne[b][2]);
+                    for (var b = 0; b < Ne.length; b++) {
+                        0 == (p = Ne[b][3]) && xe && (p = se.length),
+                            E.fillStyle = me[p].replace("0.2", Ne[b][2]);
                         var x = 10 * Ne[b][0] * v
                             , w = 20 * Ne[b][1] * v;
-                        if (tt["showothercurs"]["checked"] && m) {
+                        if (tt.showothercurs.checked && m) {
                             var M = Pe.get(Ne[b][4]);
-                            null != M && M["highlighted"] && (E.lineWidth = 3 * v,
-                                E["strokeStyle"] = p == se["length"] ? "#FFFFFF" : se[p],
-                                E["beginPath"](),
-                                E["moveTo"](Math["round"](10 * M["rawx"] * v + r / 2), Math["round"](20 * M["rawy"] * v + a)),
-                                E["lineTo"](Math["round"](x + r / 2), Math.round(w + a)),
+                            null != M && M.highlighted && (E.lineWidth = 3 * v,
+                                E.strokeStyle = p == se.length ? "#FFFFFF" : se[p],
+                                E.beginPath(),
+                                E.moveTo(Math.round(10 * M.rawx * v + r / 2), Math.round(20 * M.rawy * v + a)),
+                                E.lineTo(Math.round(x + r / 2), Math.round(w + a)),
                                 E.stroke())
                         }
-                        E["fillRect"](x, w, r, a)
+                        E.fillRect(x, w, r, a)
                     }
                     var anonIdShow = document.getElementById('anonIdShow').checked;
-                    if (E["fillStyle"] = be,
-                        kt(y = Math["round"](10 * Ce["rawx"] * v), g = Math["round"](20 * Ce["rawy"] * v), r, a),
-                        tt["shownametags"].checked && (gt(E),
-                            Mt((tt["anonymous"]["checked"] || je == "") && anonIdShow ? `(48773355)` : (tt["anonymous"]["checked"] ? "" : je), y, g, o)),
-                        Je && $e["start"] && $e["end"]) {
+                    if (E.fillStyle = be,
+                        kt(y = Math.round(10 * Ce.rawx * v), g = Math.round(20 * Ce.rawy * v), r, a),
+                        tt.shownametags.checked && (gt(E),
+                            Mt((tt.anonymous.checked || je == "") && anonIdShow ? `(48773355)` : (tt.anonymous.checked ? "" : je), y, g, o)),
+                        Je && $e.start && $e.end) {
                         E.fillStyle = "rgba(0,120,212,0.5)",
-                            y = Math["round"](10 * Math["min"]($e["start"].x, $e.end.x) * v),
-                            g = Math.round(20 * Math.min($e["start"].y, $e["end"].y) * v);
-                        var S = Math.round(10 * Math.max($e["start"].x, $e["end"].x) * v - y + 10 * v)
-                            , I = Math.round(20 * Math["max"]($e["start"].y, $e["end"].y) * v - g + 20 * v);
-                        E["fillRect"](y, g, S, I)
+                            y = Math.round(10 * Math.min($e.start.x, $e.end.x) * v),
+                            g = Math.round(20 * Math.min($e.start.y, $e.end.y) * v);
+                        var S = Math.round(10 * Math.max($e.start.x, $e.end.x) * v - y + 10 * v)
+                            , I = Math.round(20 * Math.max($e.start.y, $e.end.y) * v - g + 20 * v);
+                        E.fillRect(y, g, S, I)
                     }
                     if (Ve || Ze) {
-                        E["fillStyle"] = Ve && Ze ? "rgba(195,219,224,0.5)" : e(Ve ? 665 : 516);
+                        E.fillStyle = Ve && Ze ? "rgba(195,219,224,0.5)" : e(Ve ? 665 : 516);
                         var C = 20 * Math.floor(Te.x / 20)
-                            , T = 10 * Math["floor"](Te.y / 10);
+                            , T = 10 * Math.floor(Te.y / 10);
                         E.fillRect(10 * C * v, 20 * T * v, 200 * v, 200 * v)
                     }
                 }(),
