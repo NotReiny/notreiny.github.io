53a44e59c80caae928a0a2c80eb50de6ef0f9eeb
NotReiny on Minecraft <142988995+NotReiny@users.noreply.github.com>
2026-04-03 10:01:25 +0700

    Update deob-readable2.js

--- static/deob-readable2.js
+++ static/deob-readable2.js
@@ -3498,7 +3498,7 @@ window.requestAnimationFrame((function e() {
 										u = void 0,
 										c = n,
 										l = 20 * Math.floor(i[0] / 20) + "," + 10 * Math.floor(i[1] / 10),
-										(u = we.get(l)) && u.protected && 0 == j) || Mt(t.n != "" || !anonIdShow ? t.n : `(0)`, y, g, o)
+										(u = we.get(l)) && u.protected && 0 == j) || Mt(t.n != "" || !anonIdShow ? t.n : `(${t.id || 0})`, y, g, o)
 							}
 						}
 					}
